be77f606b5a1087ab4aa8e4a9d5edf4ee00d30b7
Benjamin-Deak <deakwben@gmail.com>
2025-04-17 17:38:45 -0700

    Completed prompt 1

---
+++ code_journal_1/prompt_1/print_info.py
@@ -0,0 +1,6 @@
+def main():
+    print("\nName: Benjamin Deak")
+    print("Pronouns: He/Him")
+    print("My favorite movie would probably be Amelie. And my favorite food is beef stroganoff.\n")
+
+main()
